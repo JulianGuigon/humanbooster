0576116373331844bc06871603330d20d9c18d54
Julian <julian.guigon@free.fr>
2018-12-14 10:39:38 +0100

    Added basic comportment to classes : Connected, Role, Address, Admin, User, Visitor, RankingSingleton

--- TopAidi/src/com/topaidi/model/Address.java
+++ TopAidi/src/com/topaidi/model/Address.java
@@ -1,5 +1,68 @@
  	package com.topaidi.model;
 
 public class Address {
+	private String country;
+	private String city;
+	private Integer postalCode;
+	private String wording;
+	private Integer streetNumber;
+	
+	public Address() {
+		// TODO Auto-generated constructor stub
+	}
 
+	public Address(String country, String city, Integer postalCode, String wording, Integer streetNumber) {
+		super();
+		this.country = country;
+		this.city = city;
+		this.postalCode = postalCode;
+		this.wording = wording;
+		this.streetNumber = streetNumber;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public Integer getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(Integer postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getWording() {
+		return wording;
+	}
+
+	public void setWording(String wording) {
+		this.wording = wording;
+	}
+
+	public Integer getStreetNumber() {
+		return streetNumber;
+	}
+
+	public void setStreetNumber(Integer streetNumber) {
+		this.streetNumber = streetNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [country=" + country + ", city=" + city + ", postalCode=" + postalCode + ", wording=" + wording
+				+ ", streetNumber=" + streetNumber + "]";
+	}
 }
--- TopAidi/src/com/topaidi/model/roles/Admin.java
+++ TopAidi/src/com/topaidi/model/roles/Admin.java
@@ -1,5 +1,54 @@
 package com.topaidi.model.roles;
 
-public class Admin {
+import com.sun.jndi.cosnaming.IiopUrl.Address;
+import com.topaidi.abstracts.Connected;
+import com.topaidi.model.Category;
+import com.topaidi.model.Comment;
+import com.topaidi.model.Idea;
 
+public class Admin extends Connected {
+	
+	public Admin() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Admin(int id, String name, String email, String password, Address address, Integer phoneNumber,
+			String secretQuestion, String secretAnswer, String element) {
+		super(id, name, email, password, address, phoneNumber, secretQuestion, secretAnswer);
+	}
+	
+	public boolean ban(User user) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public Category createCategory() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public boolean desactivate(Idea idea) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public boolean desactivate(User user) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public boolean desactivate(Comment comment) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public boolean deleteCategory(Category category) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public boolean validSubscription(User user) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public String toString() {
+		return "Admin [getName()=" + getName() + ", getEmail()=" + getEmail() + ", getPassword()=" + getPassword()
+				+ ", getAddress()=" + getAddress() + ", getPhoneNumber()=" + getPhoneNumber() + ", getSecretQuestion()="
+				+ getSecretQuestion() + ", getSecretAnswer()=" + getSecretAnswer() + ", getId()=" + getId() + "]";
+	}
 }
