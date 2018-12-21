6cd2bae8c0825fb5a1a2354b7d603f67c20531f3
Julian <julian.guigon@free.fr>
2018-12-21 10:11:09 +0100

    Added one test

---
+++ TopAidi/src/test/java/com/topaidi/model/IdeaTest.java
@@ -0,0 +1,19 @@
+package com.topaidi.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IdeaTest {
+
+	@Test
+	public void testGetIdeaById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIdeaSortByDateCreate() {
+		fail("Not yet implemented");
+	}
+
+}
