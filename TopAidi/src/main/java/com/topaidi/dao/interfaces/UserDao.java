a87df46a4d05c6cf6253b7eae8e0d67381faefdb
Alexandre <alexandre.benkheira@gmail.com>
2019-01-12 13:17:11 +0100

    Delete project

--- TopAidi/src/main/java/com/topaidi/dao/interfaces/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.topaidi.dao.interfaces;
-
-import com.topaidi.model.roles.User;
-
-public interface UserDao extends GenericDao<User,Integer> {
-
-}
--- TopAidi/src/main/java/com/topaidi/enums/AlertType.java
+++
@@ -1,6 +0,0 @@
-package com.topaidi.enums;
-
-public enum AlertType {
-	Comment,
-	Idea
-}
