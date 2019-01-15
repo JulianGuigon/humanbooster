4fe4f3ca26ec7e6b1464d022b3a40c0c7c8abd05
Alexandre <alexandre.benkheira@gmail.com>
2019-01-15 15:24:06 +0100

    test commit for profil

--- topaidi/src/main/java/com/topaidi/dao/AddressDaoJpa.java
+++ topaidi/src/main/java/com/topaidi/dao/AddressDaoJpa.java
@@ -26,7 +26,7 @@ public class AddressDaoJpa implements AddressDao {
 	public void deleteByKey(Integer key) {
 		em.remove(findByKey(key));
 	}
-	
+	//this is comment
 	@Override
 	@SuppressWarnings("unchecked")
 	public List<Address> findAll() {
